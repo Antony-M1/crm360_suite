f0b53efdfd827cacda456765f0693b02c370ae8c
Antony Praveenkumar M <antonykumar15898@gmail.com>
2023-09-22 12:50:26 +0530

    Templates correction

--- crm360_suite/settings.py
+++ crm360_suite/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'crm360_suite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates/crm'],
+        'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
